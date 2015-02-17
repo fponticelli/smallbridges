12463a4a87206a12cefa8865445a13fca8901226
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-16 22:23:09 -0700

    added more files to stl generation

--- build-stl.sh
+++ build-stl.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-for i in "bearing" "ob_wheel" "shim" "vslot"
+for i in "bearing" "brackets" "ob_spacer" "ob_wheel" "or_gantry_plate" "shim" "vslot" "vslot_gantry_plate"
 do
-  #echo "Exporting $i.scad to .stl"
-  echo "include <scad/$i.scad>; ${i}_test();" > temp.scad
+  echo "\nExporting $i.scad to .stl\n=============================================\n"
+  echo "include <scad/$i.scad>\n\$fn = 48;\n${i}_test();" > temp.scad
   openscad -o "stl/$i.stl" temp.scad
 done
+
+echo "\nExporting printer.scad to .stl\n=============================================\n"
+echo "include <resin/printer.scad>\n\$fn = 48;\n\$detailed = true;\nrotate([90,0,0]) {\n  printer();\n}" > temp.scad
+openscad -o "stl/$i.stl" temp.scad
 rm temp.scad
